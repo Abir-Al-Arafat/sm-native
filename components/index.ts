965f33eca31a6501c4008591605f97b2b78914a6
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-09-28 17:48:47 +0600

    add: component index root file

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// Export all components for easy importing
+export { ActionButton } from "./Buttons/ActionButton";
+export { Header } from "./Headers/Header/Header";
+export { UserHeader } from "./Headers/UserHeader/UserHeader";
+export { CreatePost } from "./Posts/CreatePost/CreatePost";
+export { PostFeed } from "./Posts/PostFeed/PostFeed";
+export { PostItem } from "./Posts/PostItem/PostItem";
